dd4f3463dda43b1eb9116324dac051a78664170c
mariam511 <mariam.ezz2001@gmail.com>
2024-06-21 22:47:20 +0300

    fix calender

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -2,9 +2,7 @@
 <footer class="main-footer">
     <strong>
         Copyright &copy; {{ now()->year }}
-        <a href="https://www.instagram.com/mariammezz_?igsh=bGl5eWthNGFvcWsz&utm_source=qr" target="_blank">Ezzo</a>
-        &
-        <a href="https://www.instagram.com/do7aaa.9?igsh=MWI1Z2R2ZXlla21xMw==" target="_blank">Doha</a>.
+        <a href="https://www.instagram.com/do7aaa.9?igsh=MWI1Z2R2ZXlla21xMw==" target="_blank">the events catsher</a>
     </strong>
     All rights reserved.
 </footer>
--- resources/views/site/pages/aboutus/feedback.blade.php
+++ resources/views/site/pages/aboutus/feedback.blade.php
@@ -105,30 +105,26 @@
             <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample3.jpg" alt="profile-sample3"
                 class="profile" />
             <figcaption>
-                <h2>Eleanor Crisp</h2>
+                <h2>mariam hussen</h2>
                 <h4>UX Design</h4>
-                <blockquote>Dad buried in landslide! Jubilant throngs fill streets! Stunned father inconsolable - demands
-                    recount!</blockquote>
+                <blockquote> notice your innovative use of technology in the event,thank you for being great</blockquote>
             </figcaption>
         </figure>
-        <figure class="snip1390 hover"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg"
+        <figure class="snip1390 hover"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample6.jpg"
                 alt="profile-sample5" class="profile" />
             <figcaption>
-                <h2>Gordon Norman</h2>
+                <h2>mariam nabil</h2>
                 <h4>Accountant</h4>
-                <blockquote>Wormwood : Calvin, how about you? Calvin : Hard to say ma'am. I think my cerebellum has just
-                    fused.
+                <blockquote>it was an amazing trip ,hoping the next one also like that
                 </blockquote>
             </figcaption>
         </figure>
         <figure class="snip1390"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample6.jpg"
                 alt="profile-sample6" class="profile" />
             <figcaption>
-                <h2>Sue Shei</h2>
+                <h2>doaa rashad</h2>
                 <h4>Public Relations</h4>
-                <blockquote>The strength to change what I can, the inability to accept what I can't and the incapacity to
-                    tell
-                    the difference.</blockquote>
+                <blockquote>Your use of visual aids enhances students’ understanding and engagement throughout the session. Keep it up</blockquote>
             </figcaption>
         </figure>
     </div>
